3f2e4459727848da7114fdd40dd4cb77bbd10bbf
Artem-Grushchinskiy <122301492+Artem-Grushchinskiy@users.noreply.github.com>
2023-04-12 14:48:55 +0200

    finaly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+// импорты ============
+import axios from 'axios';
+import Notiflix from 'notiflix';
+// переменные =========
 let input;
 let currentPage = 1;
 let totalPages = 1;
+
+// поиски и связки ===============
+const btnLoadMore = document.querySelector(`.js-load-more`);
+const sectionRender = document.querySelector(`.gallery`);
+const formSearch = document.querySelector(`#search-form`);
+btnLoadMore.addEventListener(`click`, nextPage);
+formSearch.addEventListener('submit', onFormSubmit);
+
+// запрос ====================
 function fetchImg(input, currentPage) {
   const params = {
     key: '35306825-77139744492b195714f08c4a6',
@@ -12,34 +25,43 @@ function fetchImg(input, currentPage) {
     per_page: 40,
   };
   const urlParams = new URLSearchParams(params);
-  return fetch(`https://pixabay.com/api/?${urlParams}`).then(response => {
-    return response.json();
-  });
+  return axios.get(`https://pixabay.com/api/?${urlParams}`);
+  // //   return fetch(`https://pixabay.com/api/?${urlParams}`).then(response => {
+  // //     return response.json();
+  //   });
 }
-const btnLoadMore = document.querySelector(`.js-load-more`);
-btnLoadMore.addEventListener(`click`, nextPage);
-const sectionRender = document.querySelector(`.gallery`);
-const formSearch = document.querySelector(`#search-form`);
-formSearch.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(event) {
+// форма страница карточка ==============
+async function onFormSubmit(event) {
   event.preventDefault();
   input = event.target[0].value;
   currentPage = 1;
-  fetchImg(input, currentPage).then(response => {
+  try {
+    const response = await fetchImg(input, currentPage);
     sectionRender.innerHTML = ``;
-    renderImgs(response.hits);
-    totalPages = Math.ceil(response.totalHits / 40);
+
+    totalPages = Math.ceil(response.data.totalHits / 40);
+    if (response.data.totalHits === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      btnLoadMore.classList.add(`hide`);
+      return;
+    }
     if (totalPages > 1) {
       btnLoadMore.classList.remove(`hide`);
     } else {
       btnLoadMore.classList.add(`hide`);
     }
-  });
+    renderImgs(response.data.hits);
+  } catch (error) {
+    console.log(error);
+  }
 }
 function renderImgs(cards) {
-  const markUp = cards.map(elem => {
-    return `<div class="photo-card">
+  const markUp = cards
+    .map(elem => {
+      return `<div class="photo-card">
         <img src="${elem.webformatURL}" alt="${elem.tags}" loading="lazy" />
         <div class="info">
           <p class="info-item">
@@ -60,15 +82,21 @@ function renderImgs(cards) {
           </p>
         </div>
       </div>`;
-  });
+    })
+    .join(``);
   sectionRender.insertAdjacentHTML(`beforeend`, markUp);
 }
-function nextPage(event) {
+
+// пагинация ======================
+async function nextPage(event) {
   currentPage += 1;
-  if (currentPage === totalPages) {
-    btnLoadMore.classList.add(`hide`);
+  try {
+    if (currentPage === totalPages) {
+      btnLoadMore.classList.add(`hide`);
+    }
+    const response = await fetchImg(input, currentPage);
+    renderImgs(response.data.hits);
+  } catch (error) {
+    console.log(error);
   }
-  fetchImg(input, currentPage).then(response => {
-    renderImgs(response.hits);
-  });
 }
